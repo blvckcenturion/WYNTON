fac261171df5142ae1699652eab323ce7a28b28c
BLVCKCENTURION <blvckcenturion@gmail.com>
2023-06-28 16:05:48 -0400

    fix: build issues related to DB

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -2,12 +2,11 @@ extern crate dotenv;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
-use std::env;
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = "/Users/santiagosarabia/Dev/projects/WYNTON/wynton.db";
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
