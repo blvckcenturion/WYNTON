2a78673a776e7a1ea7b1f138ff18d692a2255b16
BLVCKCENTURION <blvckcenturion@gmail.com>
2023-08-11 21:50:00 -0400

    feat: implemented stats for daily orders

--- src/modules/orders/DailyOrders.tsx
+++ src/modules/orders/DailyOrders.tsx
@@ -3,10 +3,14 @@ import orderService from "./services/orders";
 import convertUTCDateToLocalDate from "../utils/functions/convertUTCDateToLocalDate";
 import capitalize from "../utils/functions/capitalize";
 
-const DailyOrders = ({user}: {user: any}) => { 
+const DailyOrders = ({ user }: { user: any }) => { 
+    
     const [dailyOrders, setDailyOrders] = useState<any[]>([])
     const [status, setStatus] = useState<any>("")
     const [orderType, setOrderType] = useState<any>("")
+    const [totalGenerated, setTotalGenerated] = useState<number>(0);
+    const [totalOrders, setTotalOrders] = useState<number>(0);
+    const [totalProducts, setTotalProducts] = useState<number>(0);
 
     useEffect(() => {
         (async () => {
@@ -14,35 +18,32 @@ const DailyOrders = ({user}: {user: any}) => {
             let finalizedOrders: any[] = await orderService.load(2)
             
             let dailyOrders: any[] = [...canceledOrders, ...finalizedOrders]
-
+    
             dailyOrders = dailyOrders.filter((order: any) => {
                 return order.user_id === user.id
-            })
-            console.log(dailyOrders)
+            });
+    
             dailyOrders = dailyOrders.filter((order: any) => {
                 let today = convertUTCDateToLocalDate(new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())));
                 let orderDate = convertUTCDateToLocalDate(new Date(order.created_at))
                 today = new Date(today.getFullYear(), today.getMonth(), today.getDate())
                 return orderDate >= today
-            })
-
+            });
+    
             dailyOrders = dailyOrders.filter((order: any) => {
-                // ... existing code ...
-
                 // Filter by status if specified
                 if (status !== "" && status !== null) {
                     return order.status == parseInt(status);
                 }
-
+    
                 // Filter by order type if specified
                 if (orderType !== "" && orderType !== null) {
                     return order.order_type == parseInt(orderType);
                 }
-
+    
                 return true;
-            })
-
-
+            });
+    
             dailyOrders = dailyOrders.map((order: any) => { 
                 return {
                     id: order.id,
@@ -65,11 +66,41 @@ const DailyOrders = ({user}: {user: any}) => {
                     status: order.status,
                     dateTime: convertUTCDateToLocalDate(new Date(order.created_at)),
                 }
-            })
-            setDailyOrders(dailyOrders)
-            
-        })()
-    }, [status, orderType])
+            });
+    
+            setDailyOrders(dailyOrders);
+    
+            // Filter orders with status 2 for statistics calculation
+            const finalizedDailyOrders = dailyOrders.filter(order => order.status === 2);
+    
+            // Calculate the statistics
+            const totalGen = finalizedDailyOrders.reduce((acc, order) => acc + order.items.reduce((acc:any, item:any) => acc + (item.price * item.quantity), 0), 0);
+            const totalOrd = finalizedDailyOrders.length;
+            const totalProd = finalizedDailyOrders.reduce((acc, order) => acc + order.items.reduce((acc:any, item:any) => acc + item.quantity, 0), 0);
+    
+            setTotalGenerated(totalGen);
+            setTotalOrders(totalOrd);
+            setTotalProducts(totalProd);
+    
+            console.log(totalGen);
+            console.log(totalOrd);
+            console.log(totalProd);
+        })();
+    }, [status, orderType]);
+    
+
+    // Filter orders with status 2 for statistics calculation
+    const finalizedDailyOrders = dailyOrders.filter(order => order.status === 2);
+    const canceledDailyOrders = dailyOrders.filter(order => order.status === 0);
+
+    // Calculate the statistics for finalized orders
+    const totalGenFinalized = finalizedDailyOrders.reduce((acc, order) => acc + order.items.reduce((acc:any, item:any) => acc + (item.price * item.quantity), 0), 0);
+    const totalOrdFinalized = finalizedDailyOrders.length;
+    const totalProdFinalized = finalizedDailyOrders.reduce((acc, order) => acc + order.items.reduce((acc:any, item:any) => acc + item.quantity, 0), 0);
+
+    // Calculate the statistics for canceled orders
+    const totalOrdCanceled = canceledDailyOrders.length;
+    
 
     const handleStatusChange = (e: any) => { 
         setStatus(e.target.value)
@@ -83,6 +114,7 @@ const DailyOrders = ({user}: {user: any}) => {
     return (
         <div className="daily-orders-module">
             <h1>Ordenes del dia</h1>
+            
             <div className="filters">
                 <label htmlFor="orderStatus">
                     Estado
@@ -151,6 +183,25 @@ const DailyOrders = ({user}: {user: any}) => {
                     </table>
                 )}
             </div>
+            <div className="stats-bar">
+                <div className="stat-card">
+                    <h2>Total Generado</h2>
+                    <p>{totalGenFinalized.toFixed(2)} BS</p>
+                </div>
+                <div className="stat-card">
+                    <h2>Ordenes Finalizadas</h2>
+                    <p>{totalOrdFinalized}</p>
+                </div>
+                <div className="stat-card">
+                    <h2>Ordenes Canceladas</h2>
+                    <p>{totalOrdCanceled}</p>
+                </div>
+                <div className="stat-card">
+                    <h2>Productos Vendidos</h2>
+                    <p>{totalProdFinalized}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
