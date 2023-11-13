afc5c3ffb5c4e6757b4996bafc3ea5eec22dedbf
BLVCKCENTURION <63882089+blvckcenturion@users.noreply.github.com>
2023-11-13 18:03:38 -0400

    date conversion adjustment

--- src/modules/utils/functions/convertUTCDateToLocalDate.ts
+++ src/modules/utils/functions/convertUTCDateToLocalDate.ts
@@ -1,16 +1,10 @@
-function isDST(date: Date) {
-  const jan = new Date(date.getFullYear(), 0, 1).getTimezoneOffset();
-  const jul = new Date(date.getFullYear(), 6, 1).getTimezoneOffset();
-  return Math.max(jan, jul) !== date.getTimezoneOffset(); 
-}
-
 function convertUTCDateToLocalDate(date: Date) {
-  const newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+  const newDate = new Date(date.getTime());
 
-  const offset = isDST(date) ? 4 : 5;
-  const hours = date.getUTCHours();
-
-  newDate.setHours(hours - offset);
+  newDate.setUTCHours(date.getUTCHours()-4);
+  newDate.setUTCMinutes(date.getUTCMinutes());
+  newDate.setUTCSeconds(date.getUTCSeconds());
+  newDate.setUTCMilliseconds(date.getUTCMilliseconds());
 
   return newDate;
 }
